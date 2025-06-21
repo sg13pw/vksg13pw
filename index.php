83c8d294a04c620824e007031192648cf2d2909b
SG13PW <admin@sg13.pw>
2025-06-21 18:09:12 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,3 @@
-<form id="vk" method="post" action="https://vk.com/sg13pw">
+<form id="vk" method="post" action="https://vk.com/sg13pw"></form>
     <input type="hidden" name="hidden" value="https://vk.com/sg13pw">
-</form>
+
