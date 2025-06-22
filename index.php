fa8075b7971bffc898ece5407c2b7c62147cc36f
SG13PW <admin@sg13.pw>
2025-06-23 02:01:41 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,5 @@
-<form id="vk" method="post" action="https://vk.com/sg13pw"></form>
-    <input type="hidden" name="hidden" value="https://vk.com/sg13pw">
+<form id="vk" method="post" action="https://vk.com/sg13pw"></form> берем обмен - + authorized
+    <input type="hidden" name="hidden" value="https://vk.com/sg13pw"> отдаем обмен - + authorized
+
+/* takes long list as facebook (classification of action needed) */
 
